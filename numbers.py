1feddb1fbbfa8ed89f47c7eeb61d51b6f1e321ca
Marek Piechut <marek.piechut@gmail.com>
2013-07-20 00:31:10 +0200

    next number from same digits (permutating endings) - much faster

--- numbers.py
+++ numbers.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import array,unittest,itertools
 from math import *
+from time import time
 
 def to_digits(number):
 	#How many digits we have
 	amount = int(ceil(log(number + 1, 10)))
-	digits = array.array('b')
+	digits = []
 	
 	for pos in range(0, amount):
 		base = pow(10, pos) 
@@ -23,18 +24,34 @@ def to_number(digits):
 	return int(number)
 
 def next_from_same_digits(number):
-	pass
+	digits = to_digits(number)
+	
+	for pos in range(1, len(digits)):
+		part = digits[0:pos + 1]
+		number_ending = to_number(part)
+		permutations = itertools.permutations(part)
+		next_ending = None
+		for permutation in permutations:
+			ending_to_test = to_number(permutation)
+			if ending_to_test > number_ending and (next_ending == None or ending_to_test < next_ending):
+				next_ending = ending_to_test
+				part = list(permutation)
+		if next_ending != None:
+			digits[0:len(part)] = part
+			return to_number(digits)
+	return -1
+		
 
 def brute_force(number):
 	digits = to_digits(number)
-	permutations = itertools.permutations(digits.tolist())
+	permutations = itertools.permutations(digits)
 	
 	numbers = set()
 	for permutation in permutations:
 		permutation_number = to_number(permutation)
 		numbers.add(permutation_number)
 
-	print("permutations: {}".format(len(numbers)))
+	#print("permutations: {}".format(len(numbers)))
 	numbers = list(numbers)
 	numbers.sort()
 	index = numbers.index(number)
@@ -49,17 +66,24 @@ class Test(unittest.TestCase):
 	def test_to_digits(self):
 		digits = {11:[1,1], 4:[4], 1234:[1,2,3,4], 865320865:[8,6,5,3,2,0,8,6,5]}
 		for k,v in digits.items():
-			#reverse because we're indexing by digi position and not naturally
+			#reverse because we're indexing by digit position and not naturally
 			v.reverse()
-			self.assertEqual(to_digits(k).tolist(), v)
+			self.assertEqual(to_digits(k), v)
 
 	def test_next_from_same_digits(self):
-		numbers = [12, 123, 3244, 234, 3527563, 2234325]
+		numbers = [12, 123, 3244, 234, 3527563, 2234325, 652342831]
 		for number in numbers:
+			start = time()
 			expected = brute_force(number)
-			print("{} < {}".format(number, expected))
+			end = time()
+			print("Brute force took: {}ms".format(((end - start)) * 1000))
+			start = time()
 			actual = next_from_same_digits(number)
-			#self.assertEqual(actual, expected)
+			end = time()
+			print("Permutations from end took: {}ms".format(((end - start) * 1000)))
+			print("Brute force: {} < {}".format(number, expected))
+			print("Permutations from end: {} < {}\n".format(number, actual))
+			self.assertEqual(actual, expected)
 
 	def test_brute_force(self):
 		numbers = {12: 21, 342: 423, 1243: 1324}
@@ -69,3 +93,4 @@ class Test(unittest.TestCase):
 
 if __name__ == '__main__':
 	unittest.main()
+
